0d6f7d868244e6d648ac1d84a6b3ec22cb2095d9
omersalihotaci <2022555051@ogr.cu.edu.tr>
2026-01-15 23:57:40 +0300

    Add Netlify app to allowed CORS origins

--- backend/src/main/java/com/otaci/inatback/config/SecurityConfig.java
+++ backend/src/main/java/com/otaci/inatback/config/SecurityConfig.java
@@ -53,7 +53,8 @@ public class SecurityConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5176"));
+        config.setAllowedOrigins(List.of("http://localhost:5176",
+                                        "https://otacicomputer.netlify.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
