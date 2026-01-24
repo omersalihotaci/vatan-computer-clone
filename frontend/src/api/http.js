931ef686b684208e4c03609d0b81ba0a37774ce5
omersalihotaci <2022555051@ogr.cu.edu.tr>
2026-01-24 15:34:05 +0300

    Update http.js

--- frontend/src/api/http.js
+++ frontend/src/api/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const http = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: "https://vatan-computer-clone.onrender.com/api",
     headers: {
         "Content-Type": "application/json",
         "Cache-Control": "no-cache",
