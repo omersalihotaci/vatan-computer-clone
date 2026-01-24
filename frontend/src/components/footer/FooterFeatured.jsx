38993b8d2cd97a5a83547972e3d0a226208c93ea
omersalihotaci <2022555051@ogr.cu.edu.tr>
2026-01-24 16:31:04 +0300

    Fix incorrect redirect URL

--- frontend/src/components/footer/FooterFeatured.jsx
+++ frontend/src/components/footer/FooterFeatured.jsx
@@ -20,12 +20,14 @@ export default function FooterFeatured() {
             <h3 className="font-semibold mb-4">Öne Çıkan Ürünler</h3>
 
             <div
-                className="grid grid-cols-2 gap-6 text-sm text-gray-600"
+                className="grid grid-cols-2 gap-6 text-sm text-gray-600
+                 [&_a]:cursor-pointer
+                [&_a]:hover:underline"
             >
                 <ul className="space-y-2">
                     {leftColumn.map((product) => (
                         <li key={product.id}>
-                            <Link to={`/products/${product.id}`}>
+                            <Link to={`/product/${product.id}`}>
                                 {product.name}
                             </Link>
                         </li>
@@ -35,7 +37,7 @@ export default function FooterFeatured() {
                 <ul className="space-y-2">
                     {rightColumn.map((product) => (
                         <li key={product.id}>
-                            <Link to={`/products/${product.id}`}>
+                            <Link to={`/product/${product.id}`}>
                                 {product.name}
                             </Link>
                         </li>
